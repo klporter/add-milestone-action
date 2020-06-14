e8780dbaa012b59d7fc756956b4103a0d0a971ec
Kevin Porter <kevin.porter@salesforce.com>
2020-06-14 11:47:29 -0700

    debugging

--- dist/index.js
+++ dist/index.js
@@ -6923,7 +6923,7 @@ module.exports = isPlainObject;
 const core = __webpack_require__(872);
 const milestone = __webpack_require__(173);
 const issue = __webpack_require__(241);
-const githubContext = process.env.GITHUB_CONTEXT
+const githubContext = process.env.GH_CONTEXT
 const repo = process.env.GITHUB_REPOSITORY
 
 try {
